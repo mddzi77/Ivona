03badb3670b7db3a1358286ef96596ab09db46d8
AdmSko <adam0902000@gmail.com>
2023-12-27 23:40:25 +0100

    -initial commit

---
+++ MainScreen/MainScreen.py
@@ -0,0 +1,39 @@
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.filechooser import FileChooserListView
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGridLayout, self).__init__(**kwargs)
+        self.cols = 2
+        # Elements
+        labelName = Label(text='Ivona',font_size=35)
+        buttonNewProfile = Button(text='New Profile',size_hint_y=None, height=50)
+        buttonProfile = Button(text='Profile',size_hint_y=None, height=50)
+        buttonExit = Button(text='Exit', size_hint_y=None, height=50)
+        # BoxLayout for Buttons - LEFT
+        button_layout = BoxLayout(orientation='vertical',spacing=90,padding=15, size_hint_x=None,width=350)
+        button_layout.add_widget(labelName)
+        button_layout.add_widget(buttonNewProfile)
+        button_layout.add_widget(buttonProfile)
+        button_layout.add_widget(buttonExit)
+
+        # BoxLayout for Text Panel - RIGHT
+        text_layout = BoxLayout(orientation='vertical', spacing=15, padding=15)
+        self.text_input = TextInput(multiline=True)
+        text_layout.add_widget(self.text_input)
+
+        # Drop Load File
+        file_chooser = FileChooserListView( height=100)
+        file_chooser.bind(on_dropfile=self.on_file_drop)
+
+        # Add Widgets to the Grid
+        self.add_widget(button_layout)
+        self.add_widget(text_layout)
+        #self.add_widget(file_chooser)
+
+    def on_file_drop(self, instance, x, y, filepath):
+        # Handling Dropped File                         //Wez tu wjeb te walki z pdf itd. najlepiej w osobnym pliku .py zeby nie nasrac tu
+        print(f'Dropped file boys: {filepath}')
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# kocham plote
+from MainScreen.MainScreen import MyGridLayout
+from kivy.app import App
+from kivy.config import Config
+
+Config.set('graphics', 'Width', '800')
+Config.set('graphics', 'height', '600')
+
+
+
+class MyApp(App):
+    icon = 'custom-kivy-icon.png'
+    title = 'Basic Application'
+
+    def build(self):
+        return MyGridLayout()
+
+if __name__ == "__main__":
+    MyApp().run()
