17543e2b54f9aac206fe69134d045f9ccec22886
mddzi <md.dziem@gmail.com>
2024-01-24 20:21:42 +0100

    - quick cleanup

--- text_to_speech.py
+++ text_to_speech.py
@@ -9,25 +9,26 @@ class ElevenLabs(TextToSpeechInterface):
         self.__model = 'eleven_multilingual_v2'
         self.__api_key = open('api_key.txt', 'r').read()
         self.audio = None
+        self.name = 'Name'
         self.__text: str = ''
-        self.__voice: str | Voice = None
         self.__recordings = []
         set_api_key(self.__api_key)
 
     def set_text(self, text: str):
         self.__text = text
 
-    def set_voice(self, name: str | Voice = None, id: int = None):
-        pass
-
     def set_recordings(self, recordings: list):
         self.__recordings = recordings
 
     def clone(self, name: str, description=None):
-        v = clone(name=name, description=description, files=self.__recordings, api_key=self.__api_key)
-        self.__voice = v
+        self.voices_list.append(
+            clone(name=name, description=description, files=self.__recordings, api_key=self.__api_key)
+        )
+        # TODO: Add voice id
+
+    def generate(self):
+        self.audio = generate(self.__text, self.__api_key, self.__voice, self.__model)
 
     def play(self):
         self.audio = generate(self.__text, self.__api_key, self.__voice, self.__model)
         play(self.audio, use_ffmpeg=False)
-
--- tts_Interface.py
+++ tts_Interface.py
@@ -1,18 +1,15 @@
-from elevenlabs import Voice
-
-
 class TextToSpeechInterface:
 
     def set_text(self, text: str):
-        pass
-
-    def set_voice(self, name: str | Voice):
         pass
 
     def set_recordings(self, recordings: list):
         pass
 
     def clone(self, name: str, description=None):
+        pass
+
+    def generate(self):
         pass
 
     def play(self):
